0a7ec8c045d54f3728a65b15c1b1c52b165748a7
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-19 01:12:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def apple_news():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-        
+        if event.message == "抽"
         client = ImgurClient(client_id, client_secret)
         images = client.get_album_images(album_id)
         index = random.randint(0, len(images) - 1)
@@ -73,7 +73,14 @@ def handle_message(event):
         )
         line_bot_api.reply_message(
             event.reply_token, image_message)
-        
+        return 0
+		
+		else:
+		content = apple_news()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
 		
 
 
