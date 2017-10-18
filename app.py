f9b56359e8bcdb4802557e21a07cd127c22f5a74
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-19 00:44:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 
 
