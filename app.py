94ad572ef41b7196455a3a9202a6384c186dc297
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-18 23:35:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ def apple_news():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   print("event.reply_token:", event.reply_token)
-   print("event.message.text:", event.message.text) 
-        if event.message.text == "蘋果":   
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+        if event.message.text == "蘋果":
         content = apple_news()
         line_bot_api.reply_message(
             event.reply_token,
