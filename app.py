d05032bb165481ce9b42b81c1f8fff6ef0a0c64e
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-18 23:59:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -37,23 +37,7 @@ def callback():
         abort(400)
 
     return 'OK'
-def apple_news():
-    target_url = 'http://www.appledaily.com.tw/realtimenews/section/new/'
-    head = 'http://www.appledaily.com.tw'
-    print('Start parsing appleNews....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 15:
-            return content
-        if head in data['href']:
-            link = data['href']
-        else:
-            link = head + data['href']
-        content += '{}\n\n'.format(link)
-    return content
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
