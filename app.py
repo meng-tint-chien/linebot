f2a0804d67ba46e90459bc213f77b964e9d5bd18
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-19 11:12:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -92,6 +92,66 @@ def ptt_beauty():
                                              article.get('url', None))
         content += data
     return content
+	
+def get_page_number(content):
+    start_index = content.find('index')
+    end_index = content.find('.html')
+    page_number = content[start_index + 5: end_index]
+    return int(page_number) + 1
+
+
+def craw_page(res, push_rate):
+    soup_ = BeautifulSoup(res.text, 'html.parser')
+    article_seq = []
+    for r_ent in soup_.find_all(class_="r-ent"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                rate = r_ent.find(class_="nrec").text
+                url = 'https://www.ptt.cc' + link
+                if rate:
+                    rate = 100 if rate.startswith('爆') else rate
+                    rate = -1 * int(rate[1]) if rate.startswith('X') else rate
+                else:
+                    rate = 0
+                # 比對推文數
+                if int(rate) >= push_rate:
+                    article_seq.append({
+                        'title': title,
+                        'url': url,
+                        'rate': rate,
+                    })
+        except Exception as e:
+            # print('crawPage function error:',r_ent.find(class_="title").text.strip())
+            print('本文已被刪除', e)
+    return article_seq
+
+
+def crawl_page_gossiping(res):
+    soup = BeautifulSoup(res.text, 'html.parser')
+    article_gossiping_seq = []
+    for r_ent in soup.find_all(class_="r-ent"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                url_link = 'https://www.ptt.cc' + link
+                article_gossiping_seq.append({
+                    'url_link': url_link,
+                    'title': title
+                })
+
+        except Exception as e:
+            # print u'crawPage function error:',r_ent.find(class_="title").text.strip()
+            # print('本文已被刪除')
+            print('delete', e)
+    return article_gossiping_seq
 
 def ptt_gossiping():
     rs = requests.session()
@@ -139,7 +199,7 @@ def movie():
     soup = BeautifulSoup(res.text, 'html.parser')
     content = ""
     for index, data in enumerate(soup.select('ul.filmNextListAll a')):
-        if index == 20:
+        if index == 7:
             return content
         title = data.text.replace('\t', '').replace('\r', '')
         link = "http://www.atmovies.com.tw" + data['href']
